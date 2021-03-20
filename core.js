a7377fb776adc28bdf4750f36fcf92f4a0259c05
Grzegorz Pikiel <gpikiel@atlassian.com>
2021-03-20 12:44:07 +0100

    v8, module deconstruction

--- core.js
+++ core.js
@@ -1,6 +1,8 @@
 const path = require('path');
+//works like import staticin java
+const { log } = require('util');
+const v8 = require('v8');
 
-const util = require('util');
-
-util.log(path.basename(__filename));
-util.log('^ name of a current file.')
+log(path.basename(__filename));
+log('^ name of a current file.');
+log(v8.getHeapStatistics());
