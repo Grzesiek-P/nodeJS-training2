7bc69f07c0bcfa2b02b183eac197f11daa856e6b
Grzegorz Pikiel <gpikiel@atlassian.com>
2021-03-23 21:24:00 +0100

    collectAnswers exported to separate module

--- questions.js
+++ questions.js
@@ -1,16 +1,4 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-const question = (query) => {
-    return new Promise(resolve => {
-        rl.question(query, resolve);
-    })
-};
-
+const collectAnswers = require('./lib/collectAnswers');
 
 const questions = [
     "What is your name? ",
@@ -18,19 +6,6 @@ const questions = [
     "What are you going to do with nodejs? "
 ];
 
-const collectAnswers = async (questions, done) => {
-    const answers = [];
-    for (questionToAsk of questions) {
-        try {
-            const answer = await question(questionToAsk);
-            answers.push(answer);
-        } catch (error) {
-            console.log(`Error: ${error}`);
-        }
-    }
-
-    done(answers);
-};
 
 collectAnswers(questions, answers => {
     console.log('Thank you for answers! ');
